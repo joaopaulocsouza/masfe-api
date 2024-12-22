750f41e54ea4f3b4cd293f78e1685dfac76e32be
João Paulo Cardoso <joaopaulocs09@gmail.com>
2024-12-22 10:51:55 -0300

    feat: improvment personas

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import verbRoutes from "@routes/verbRoutes";
 import accetanceCriteriaRoutes from "@routes/acceptanceCriteriaRoutes";
 import personaRoutes from "@routes/personaRoutes";
 import uxCorrelationRoutes from "@routes/uxCorrelation";
-import detailsVerbACRoutes from "@routes/verbACRoutes";
 import authRoutes from "@routes/authRoutes";
 import dashboardRoutes from "@routes/dashboardRoutes";
 import cors from 'cors'
@@ -26,7 +25,6 @@ app.use('/users', userRoutes)
 app.use('/verbs', verbRoutes)
 app.use('/acceptance-criteria', accetanceCriteriaRoutes)
 app.use('/personas', personaRoutes)
-app.use('/verb-ac', detailsVerbACRoutes)
 app.use('/ux-correlation', uxCorrelationRoutes)
 app.use('/auth', authRoutes)
 app.use('/dashboard', dashboardRoutes)
