be663bca95b5bba206a256d05888c5615894e1c0
juomzinho <joaopaulocs09@gmail.com>
2024-12-24 14:18:33 -0300

    feat: dashboard finished and some improvments

--- src/controllers/dashboardController.ts
+++ src/controllers/dashboardController.ts
@@ -2,35 +2,57 @@ import { Request, Response } from "express";
 import prisma from "../config/db";
 import { verifyJWT } from "@utils/verifyJWT/verifyJWT";
 import { unauthorized } from "@utils/handleError/handleError";
+import { Garret } from "@utils/defines/defines";
+import handleResponse from "@utils/handleResponse/handleResponse";
 
 const getDashboard = async (req: Request, res: Response) => {
-    const validate = await verifyJWT(req.headers.cookie??"")
-    if(!validate){
-        res.status(401).json(unauthorized)
-    }
+    // const validate = await verifyJWT(req.headers.cookie??"")
+    // if(!validate){
+    //     res.status(401).json(unauthorized)
+    // }
     try{        
-        const uxs = await prisma.uxCorrelation.findMany({
-            take: 20,
-            select: {
-                name: true,
-                description: true,
-                persona: {
-                    select: {
-                        name: true,
-                        occupation: true
-                    }
+        const garret = await prisma.verbGarret.groupBy({
+            _count: {
+                verb_id: true
+            },
+            by: ["garret_id"]
+        })
+
+        const count_verbs = await prisma.uxCorrelation.groupBy({
+            by: ["verb_id"],
+            _count: true,
+            take: 10,
+            orderBy: {
+                _count: {
+                    verb_id: "desc"
                 }
             },
-            where: {
-                user_id: validate!
+        })
+        const resVerbs = await Promise.all(
+                     count_verbs.map(async (item) => {
+                        const verb = await prisma.verb.findUnique({where: {id: item.verb_id}})
+                        return {name: verb?.verb??"", value: item._count}
+                    })
+                )
+        const resGarret = garret.map(item => ({name: Garret[Number(item.garret_id)], value: item._count.verb_id}))
+        const uxCorrelations = await prisma.uxCorrelation.findMany({
+            take: 20,
+            select: {
+                id: true,
+                name: true,
+                dimension: true,
+                user: {
+                    select: {
+                        name: true
+                    }
+                }
             }
         })
         
-        res.status(200).json({
-            uxs,
-        })
-    }catch(e){
-        return
+        handleResponse.handleGetRes({code: 'GET-01', res, content: {garret: resGarret, verbs: resVerbs, uxCorrelations}})
+    }catch(e: any){
+        console.log(e)
+        handleResponse.handleErrorRes({code: e.code, res})
     }
 
   };
--- src/controllers/personasController.ts
+++ src/controllers/personasController.ts
@@ -1,17 +1,19 @@
 import { Request, Response } from "express";
 import prisma from "../config/db";
-import { handleError, missingField } from "@utils/handleError/handleError";
+import { handleError } from "@utils/handleError/handleError";
+import handleResponse from "@utils/handleResponse/handleResponse";
+
+const item = "Persona"
 
 const createPersona = async (req: Request, res: Response) => {
     if(!req.body.name){
-        res.status(400).json(missingField)
+        handleResponse.handleCreateRes({code: "CRT-03", res, item})
     }
     try{
-        const persona = await prisma.persona.create({data: {...req.body, user_id: "832d64d2-034f-46f2-8b5c-93f211be98e3", age: Number(req.body.age)}})
-        res.status(201).json(persona)
+        await prisma.persona.create({data: {...req.body, user_id: "832d64d2-034f-46f2-8b5c-93f211be98e3", age: Number(req.body.age)}})
+        handleResponse.handleCreateRes({code: "CRT-01", res, item})
     }catch(e: any){
-        console.log(e)
-        res.status(500).json(handleError(e))
+        handleResponse.handleErrorRes({code: e.code, res, item})
     }
 };
 
@@ -22,26 +24,20 @@ const getPersona = async (req: Request, res: Response) => {
           const persona = await prisma.persona.findUnique({where: {
               id: id.toString(),
             }})
-            if(!persona){
-                res.status(404).json({message: 'Persona não encontrada'})
-            }
-            res.status(200).json(persona)
+            handleResponse.handleGetRes({code: "GET-01", res, content: persona})
         } else if(user_id){
             const persona = await prisma.persona.findMany({
                 where: {
                     user_id: user_id.toString()
                 }
             })
-            if(!persona){
-                res.status(404).json({message: 'Nenhuma Persona foi encontrada'})
-            }
-            res.status(200).json(persona)
+            handleResponse.handleGetRes({code: "GET-01", res, content: persona})
         }else {
             const persona = await prisma.persona.findMany()
-            res.status(200).json(persona)
+            handleResponse.handleGetRes({code: "GET-01", res, content: persona})
         }
     }catch(e: any){
-        res.status(500).json(handleError(e))
+        handleResponse.handleErrorRes({code: e.code, res, item})
     }
 };
 
@@ -49,32 +45,20 @@ const getPersona = async (req: Request, res: Response) => {
 const updatePersona = async (req: Request, res: Response) => {
   try {
     const {id, ...data} = req.body
-    const persona = await prisma.persona.update({data, where: {id: id}})
-    res.status(200).json(persona)
-  } catch(e: any){88
-    res.status(500).json(handleError(e))
+    await prisma.persona.update({data, where: {id: id}})
+    handleResponse.handleCreateRes({code: "UPD-01", res})
+  } catch(e: any){
+    handleResponse.handleErrorRes({code: e.code, res, item})
   }
 };
 
 const deletePersona = async (req: Request, res: Response) => {
     const { id } = req.query
     try{
-
-      if(!id){
-          res.status(404).json({ message: 'Persona não encontrada' });
-        }
-        try {
-            const persona = await prisma.persona.delete({where: {id: id?.toString()}});
-            if (persona) {
-                res.status(200).json(persona);
-            } else {
-                res.status(404).json({ message: 'persona não encontrada' });
-            }
-        } catch (err: any) {
-            res.status(500).json({ error: err.message });
-        }
+        await prisma.persona.delete({where: {id: id?.toString()}});
+        handleResponse.handleErrorRes({code: "DEL-01", res, item})
     }catch(e: any){
-        res.status(500).json(handleError(e))
+        handleResponse.handleErrorRes({code: e.code, res, item})
     }
 }
 
