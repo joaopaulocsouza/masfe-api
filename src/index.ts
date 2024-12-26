2195b7b252198166234eaef9f1d6469b93a92b7d
João Paulo Cardoso <joaopaulocs09@gmail.com>
2024-12-26 13:57:24 -0300

    fix: origin added

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ['https://mas-fe.vercel.app', "http://localhost:5173"], 
+  origin: ['https://mas-fe.vercel.app', 'https://masfe.onrender.com'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true,
 }));
