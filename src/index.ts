7f87c07cee9ffb247dec55ee86588de3f36bdaa7
João Paulo Cardoso <joaopaulocs09@gmail.com>
2025-01-23 17:52:04 -0300

    feat: allow origin

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json()); 
 
 app.use(cors({
-  origin: [ 'https://masfe.onrender.com', 'http://localhost:5173'], 
+  origin: [ 'https://masfe.onrender.com', 'https://masfe.tec.br'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true,
 }));
